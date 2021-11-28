679283eaf4184155d835dd42a0e4d1dea70c0fa7
Dashiell Stander <dash.stander@gmail.com>
2021-11-28 14:28:40 -0800

    Axis trouble

--- diffusion/dynamics_env.py
+++ diffusion/dynamics_env.py
@@ -33,9 +33,9 @@ def rl_sample_step(
     keys = jax.random.split(key, num=5)
     clip_loss, clip_grad = clip_loss_fn(x, target, image_fn, clip_params) # Doing two forward passes on CLIP :/
     control, timestep = policy.apply(policy_params, keys[1], x, clip_grad, t, x_embed, target, {})
-    print(f'control: {control.shape}')
+    #print(f'control: {control.shape}')
     v = diff_model.apply(diff_params, keys[2], x, t, extra_args)
-    print(f'v: {v.shape}')
+    #print(f'v: {v.shape}')
     alpha, sigma = utils.t_to_alpha_sigma(t)
     v = v - control * (sigma / alpha)
     pred = x * alpha - v * sigma
