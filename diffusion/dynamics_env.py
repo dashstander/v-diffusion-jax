4311c186a71dd6817d684beaebbc7cbc68474fdd
Dashiell Stander <dash.stander@gmail.com>
2021-11-28 14:48:56 -0800

    Memory usage

--- diffusion/dynamics_env.py
+++ diffusion/dynamics_env.py
@@ -30,6 +30,7 @@ def rl_sample_step(
     clip_in = jnp.pad(x, [(0, 0), (0, 0), (extent, extent), (extent, extent)], 'edge')
     clip_in = jax.image.resize(clip_in, (*x.shape[:2], clip_size, clip_size), 'cubic')
     x_embed = image_fn(clip_params, normalize_fn((clip_in + 1) / 2))
+    del clip_in
     keys = jax.random.split(key, num=5)
     clip_loss, clip_grad = clip_loss_fn(x, target, image_fn, clip_params) # Doing two forward passes on CLIP :/
     control, timestep = policy.apply(policy_params, keys[1], x, clip_grad, t, x_embed, target, {})
@@ -38,13 +39,13 @@ def rl_sample_step(
     #print(f'v: {v.shape}')
     alpha, sigma = utils.t_to_alpha_sigma(t)
     v = v - control * (sigma / alpha)
-    pred = x * alpha - v * sigma
-    eps = x * sigma + v * alpha
+    #pred = x * alpha - v * sigma
+    #eps = x * sigma + v * alpha
     t_next = t - jnp.abs((timestep + jax.random.normal(keys[3], [1]) * 0.1))
     t_next = jnp.maximum(0, t_next)
     alpha_next, sigma_next = utils.t_to_alpha_sigma(t_next)
     adjusted_sigma = jnp.sqrt(sigma_next**2 - sigma**2)
-    x = pred * alpha_next + eps * adjusted_sigma
+    x = (x * alpha - v * sigma) * alpha_next + (x * sigma + v * alpha) * adjusted_sigma
     x = x + jax.random.normal(keys[4], x.shape) * adjusted_sigma
     control_loss = jnp.sum(jnp.square(control))/jnp.size(control)
-    return x, pred, t_next, clip_loss, control_loss
+    return x, t_next, clip_loss, control_loss
