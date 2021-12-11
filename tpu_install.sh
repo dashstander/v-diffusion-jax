e5acdc3c3848eace006fba9fe48c5a583b8d23da
Dashiell Stander <dash.stander@gmail.com>
2021-12-11 00:32:01 -0800

    TPU installation

--- tpu_install.sh
+++ tpu_install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
-
-./CLIP_JAX/tpu_install.sh
+pip3 install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip3 install git+https://github.com/openai/CLIP.git
+git clone https://github.com/kingoflolz/CLIP_JAX.git
+pip3 install -r CLIP_JAX/requirements.txt
+pip3 install -e CLIP_JAX/
 pip3 install -r requirements.txt
 pip3 install -e .
