6bdfb2e059eee037f6d3d157000a785febcedb31
Dashiell Stander <dash.stander@gmail.com>
2021-12-12 11:55:19 -0800

    ConretizationError :/

--- diffusion/models/clip_latent.py
+++ diffusion/models/clip_latent.py
@@ -1,5 +1,6 @@
 import clip_jax.model as clip
 import haiku as hk
+from haiku._src.basic import dropout
 import jax
 import jax.numpy as jnp
 from .. import utils
@@ -184,26 +185,6 @@ class CrossAttention(hk.Module):
         xy = jax.nn.relu(jnp.concatenate([x_atts, y_atts], axis=1))
         return self.fuse(xy)
 
-    
-
-class ResLayer(hk.Module):
-    
-    def __init__(self, input_size, mid_size, dr_rate, name=None):
-        super().__init__(name)
-        self.l1 = hk.Linear(mid_size)
-        self.l2 = hk.Linear(input_size)
-        self.dr_rate = dr_rate
-    
-    def __call__(self, x, is_training):
-        dr = self.dr_rate if is_training > 0.0 else 0.0
-        x1 = self.l1(x)
-        x1 = jax.nn.relu(x1)
-        x1 = hk.dropout(hk.next_rng_key(), dr, x1)
-        x1 = self.l2(x1)
-        return jax.nn.relu(x1) + x
-        
-
-
 
 class ResMLP(hk.Module):
     def __init__(self, num_layers, input_size, mid_size, dr_rate, name=None):
@@ -212,12 +193,12 @@ class ResMLP(hk.Module):
         self.input_size = input_size
         self.mid_size = mid_size
         self.dr_rate = dr_rate
+        self.layers = [hk.nets.MLP([mid_size, input_size]) for _ in range(num_layers)]
 
     def __call__(self, x, is_training):
-        dr = self.dr_rate if is_training > 0.0 else 0.0
         x_init = x
-        for _ in range(self.num_layers):
-            x = ResLayer(self.input_size, self.mid_size, dr)(x, is_training)
+        for layer in self.layers:
+            x = layer(x, dropout_rate=self.dr_rate * is_training, rng=hk.next_rng_key())
         return x + x_init
 
 
@@ -231,110 +212,118 @@ def reshape_embed_to_image(x, image_shape):
     return x[:, None].repeat(repeats, axis=1).reshape(target_shape)        
 
 
-def diffusion_image_model(x, t, y, extra_args, is_training):
-    c = 256
-    log_snr = utils.alpha_sigma_to_log_snr(*utils.t_to_alpha_sigma(t))
-    timestep_embed = FourierFeatures(64, 1)(log_snr[:, None])
 
-    # y_1 = jnp.concatenate([timestep_embed, y], axis=1)
-    # y_1 = hk.Linear(256)(y_1)
-    # y_1 = jax.nn.relu(y_1)
+class DiffusionImage(hk.Module):
 
-    y_1 = ResMLP(2, 512, 512, 0.1)(y, is_training)
-    y_1 = hk.Linear(64)(y_1)
-    te_planes = jnp.tile(timestep_embed[..., None, None], [1, 1, *x.shape[2:]])
-    y_planes = jnp.tile(y_1[..., None, None], [1, 1, *x.shape[2:]])
-    x = jnp.concatenate([x, te_planes, y_planes], axis=1)  
-    x = ResConvBlock(4, c // 2, c // 2, name="ResBlock1")(x, is_training) # Nx128x256x256
-    #print(f'x: {x.shape}')
-    ########################################################
-    x_2 = hk.AvgPool(4, 2, "SAME", 1)(x)  # Nx128x128x128
-    #print(f'x_2: {x_2.shape}')
-    x_2 = ResConvBlock(2, c, c, name="ResBlock2")(x_2, is_training)
-    ########################################################
-    x_3 = hk.AvgPool(2, 2, "SAME", 1)(x_2)  # Nx256x64x64
-    #print(f'x_3: {x_3.shape}')
-    x_3 = ResConvBlock(1, c * 2, c * 2, name="ResBlock3")(x_3, is_training)
-    ########################################################
-    x_4 = hk.AvgPool(2, 2, "SAME", 1)(x_3)  # Nx512x32x32
-    x_4 = ResConvBlock(2, c * 2, c * 2, name="ResBlock4")(x_4, is_training)
-    #print(f'x_4: {x_4.shape}')
-    ########################################################
-    x_5 = hk.AvgPool(2, 2, "SAME", 1)(x_4)  # Nx256x16x16
-    #print(f'x_5: {x_5.shape}')
-    x_5 = hk.remat(ResConvBlockAtt(4, c * 4 // 128, c * 2, c * 4, name="ResBlock5"))(x_5, is_training)
-    ########################################################
-    x_6 = hk.AvgPool(2, 2, "SAME", 1)(x_5)  # 8x8
-    #print(f'x_6: {x_6.shape}')
-    x_6 = hk.remat(ResConvBlockAtt(4, c * 4 // 128, c * 4, c * 4, name="ResBlock6"))(x_6, is_training)
-    #########################################################
-    x_7 = hk.AvgPool(2, 2, "SAME", 1)(x_6)  # Nx1024x4x4
-    #print(f'x_7: {x_7.shape}')
-    x_7 = ResConvBlock(4, c * 4, c * 4, c * 4, name="ResBlock7")(x_7, is_training)
-    x_7 = jax.image.resize(x_7, [*x_7.shape[:2], *x_6.shape[2:]], "nearest")
-    ##########################################################
-    x_6 = jnp.concatenate([x_6, x_7], axis=1)
-    x_6 = hk.remat(ResConvBlockAtt(4, c * 4 // 128, c * 4, c * 4, name="SecondResBlock6"))(x_6, is_training)
-    x_6 = jax.image.resize(x_6, [*x_6.shape[:2], *x_5.shape[2:]], "nearest")
-    ############################################################
-    x_5 = jnp.concatenate([x_5, x_6], axis=1)
-    x_5 = hk.remat(ResConvBlockAtt(
-        2, c * 4 // 128, c * 4, c * 4, c * 2, c * 2 // 128, name="SecondResBlock5"
-    ))(x_5, is_training)
-    x_5 = jax.image.resize(x_5, [*x_5.shape[:2], *x_4.shape[2:]], "nearest")
-    ##############################################################
-    x_4 = jnp.concatenate([x_4, x_5], axis=1)
-    x_4 = hk.remat(ResConvBlock(1, c * 2, c * 2, name='SecondResBlock4'))(x_4, is_training)
-    x_4 = jax.image.resize(x_4, [*x_4.shape[:2], *x_3.shape[2:]], "nearest")
-    ##############################################################
-    x_3 = jnp.concatenate([x_3, x_4], axis=1)
-    x_3 = hk.remat(ResConvBlock(1, c * 2, c * 2, c, name='SecondResBlock3'))(x_3, is_training)
-    x_3 = jax.image.resize(x_3, [*x_3.shape[:2], *x_2.shape[2:]], "nearest")
-    ##############################################################
-    x_2 = jnp.concatenate([x_2, x_3], axis=1)
-    x_2 = ResConvBlock(1, c, c, c // 2, name='SecondResBlock2')(x_2, is_training)
-    x_2 = jax.image.resize(x_2, [*x_2.shape[:2], *x.shape[2:]], "nearest")
-    ##############################################################
-    x = jnp.concatenate([x, x_2], axis=1)
-    x = hk.remat(ResConvBlock(1, c // 2, c // 2, 3, name='SecondResBlock1'))(x, is_training)
-    return x, y
+    def __init__(self, size):
+        super().__init__()
+        self.c = size
 
 
-def diffusion_latent_model(x, t, y, extra_args, is_training):
-    c = 256
-    log_snr = utils.alpha_sigma_to_log_snr(*utils.t_to_alpha_sigma(t))
-    timestep_embed = FourierFeatures(64, 1)(log_snr[:, None])
+    def __call__(self, x, t, y, extra_args, is_training):
+        log_snr = utils.alpha_sigma_to_log_snr(*utils.t_to_alpha_sigma(t))
+        timestep_embed = FourierFeatures(64, 1)(log_snr[:, None])
 
-    y_1 = jnp.concatenate([timestep_embed, y], axis=1)
-    y_1 = hk.Linear(512)(y_1)
-    y_1 = jax.nn.relu(y_1)
+        y_1 = ResMLP(2, 512, 512, 0.1)(y, is_training)
+        y_1 = hk.Linear(64)(y_1)
+        te_planes = jnp.tile(timestep_embed[..., None, None], [1, 1, *x.shape[2:]])
+        y_planes = jnp.tile(y_1[..., None, None], [1, 1, *x.shape[2:]])
+        x = jnp.concatenate([x, te_planes, y_planes], axis=1)  
+        x = ResConvBlock(4, self.c // 2, self.c // 2, name="ResBlock1")(x, is_training) # Nx128x256x256
+        #print(f'x: {x.shape}')
+        ########################################################
+        x_2 = hk.AvgPool(4, 2, "SAME", 1)(x)  # Nx128x128x128
+        #print(f'x_2: {x_2.shape}')
+        x_2 = ResConvBlock(2, self.c, self.c, name="ResBlock2")(x_2, is_training)
+        ########################################################
+        x_3 = hk.AvgPool(2, 2, "SAME", 1)(x_2)  # Nx256x64x64
+        #print(f'x_3: {x_3.shape}')
+        x_3 = ResConvBlock(1, self.c * 2, self.c * 2, name="ResBlock3")(x_3, is_training)
+        ########################################################
+        x_4 = hk.AvgPool(2, 2, "SAME", 1)(x_3)  # Nx512x32x32
+        x_4 = ResConvBlock(2, self.c * 2, self.c * 2, name="ResBlock4")(x_4, is_training)
+        #print(f'x_4: {x_4.shape}')
+        ########################################################
+        x_5 = hk.AvgPool(2, 2, "SAME", 1)(x_4)  # Nx256x16x16
+        #print(f'x_5: {x_5.shape}')
+        x_5 = hk.remat(ResConvBlockAtt(4, self.c * 4 // 128, self.c * 2, self.c * 4, name="ResBlock5"))(x_5, is_training)
+        ########################################################
+        x_6 = hk.AvgPool(2, 2, "SAME", 1)(x_5)  # 8x8
+        #print(f'x_6: {x_6.shape}')
+        x_6 = hk.remat(ResConvBlockAtt(4, self.c * 4 // 128, self.c * 4, self.c * 4, name="ResBlock6"))(x_6, is_training)
+        #########################################################
+        x_7 = hk.AvgPool(2, 2, "SAME", 1)(x_6)  # Nx1024x4x4
+        #print(f'x_7: {x_7.shape}')
+        x_7 = ResConvBlock(4, self.c * 4, self.c * 4, self.c * 4, name="ResBlock7")(x_7, is_training)
+        x_7 = jax.image.resize(x_7, [*x_7.shape[:2], *x_6.shape[2:]], "nearest")
+        ##########################################################
+        x_6 = jnp.concatenate([x_6, x_7], axis=1)
+        x_6 = hk.remat(ResConvBlockAtt(4, self.c * 4 // 128, self.c * 4, self.c * 4, name="SecondResBlock6"))(x_6, is_training)
+        x_6 = jax.image.resize(x_6, [*x_6.shape[:2], *x_5.shape[2:]], "nearest")
+        ############################################################
+        x_5 = jnp.concatenate([x_5, x_6], axis=1)
+        x_5 = hk.remat(ResConvBlockAtt(
+            2, self.c * 4 // 128, self.c * 4, self.c * 4, self.c * 2, self.c * 2 // 128, name="SecondResBlock5"
+        ))(x_5, is_training)
+        x_5 = jax.image.resize(x_5, [*x_5.shape[:2], *x_4.shape[2:]], "nearest")
+        ##############################################################
+        x_4 = jnp.concatenate([x_4, x_5], axis=1)
+        x_4 = hk.remat(ResConvBlock(1, self.c * 2, self.c * 2, name='SecondResBlock4'))(x_4, is_training)
+        x_4 = jax.image.resize(x_4, [*x_4.shape[:2], *x_3.shape[2:]], "nearest")
+        ##############################################################
+        x_3 = jnp.concatenate([x_3, x_4], axis=1)
+        x_3 = hk.remat(ResConvBlock(1, self.c * 2, self.c * 2, self.c, name='SecondResBlock3'))(x_3, is_training)
+        x_3 = jax.image.resize(x_3, [*x_3.shape[:2], *x_2.shape[2:]], "nearest")
+        ##############################################################
+        x_2 = jnp.concatenate([x_2, x_3], axis=1)
+        x_2 = ResConvBlock(1, self.c, self.c, self.c // 2, name='SecondResBlock2')(x_2, is_training)
+        x_2 = jax.image.resize(x_2, [*x_2.shape[:2], *x.shape[2:]], "nearest")
+        ##############################################################
+        x = jnp.concatenate([x, x_2], axis=1)
+        x = hk.remat(ResConvBlock(1, self.c // 2, self.c // 2, 3, name='SecondResBlock1'))(x, is_training)
+        return x
 
-    te_planes = jnp.tile(
-        timestep_embed[..., None, None], [1, 1, x.shape[2], x.shape[3]]
-    )
-    x = jnp.concatenate([x, te_planes], axis=1)  
-    x = ResConvBlock(4, c // 2, c // 2, name="ResBlock1")(x, is_training) # Nx128x256x256
-    #print(f'x: {x.shape}')
-    ########################################################
-    x_2 = hk.AvgPool(4, 2, "SAME", 1)(x)  # Nx128x128x128
-    #print(f'x_2: {x_2.shape}')
-    x_embed_2 = hk.remat(clip.VisualTransformer(128, 16, 128, 2, 2, 512, "ViT01"))(x_2)
-    ##############################################################
-    y2 = hk.Linear(512)(jnp.concatenate([y_1, x_embed_2], axis=1))
-    y_2 = ResMLP(4, 512, 1024, 0.1)(y2, is_training)
-    ############################################################
-    y_3 = ResMLP(4, 1024, 512, 0.1)(jnp.concatenate([y_2, y], axis=1), is_training)
-    y_3 = hk.Linear(512)(y_3)
-    ##############################################################
-    y_4 = ResMLP(4, 512, 1024, 0.1)(y_3, is_training)
-    y_4 = hk.Linear(512)(jnp.concatenate([y_4, y_2], axis=1))
-    y = ResMLP(2, 512, 512, 0.0)(y_4, is_training)
-    return y
+
+class DiffusionLatent(hk.Module):
+
+    def __init__(self, size):
+        super().__init__()
+        self.c = size
+
+    def __call__(self, x, t, y, extra_args, is_training):
+        log_snr = utils.alpha_sigma_to_log_snr(*utils.t_to_alpha_sigma(t))
+        timestep_embed = FourierFeatures(64, 1)(log_snr[:, None])
+
+        y_1 = jnp.concatenate([timestep_embed, y], axis=1)
+        y_1 = hk.Linear(512)(y_1)
+        y_1 = jax.nn.relu(y_1)
+
+        te_planes = jnp.tile(
+            timestep_embed[..., None, None], [1, 1, x.shape[2], x.shape[3]]
+        )
+        x = jnp.concatenate([x, te_planes], axis=1)  
+        x = ResConvBlock(4, self.c // 2, self.c // 2, name="ResBlock1")(x, is_training) # Nx128x256x256
+        #print(f'x: {x.shape}')
+        ########################################################
+        x_2 = hk.AvgPool(4, 2, "SAME", 1)(x)  # Nx128x128x128
+        #print(f'x_2: {x_2.shape}')
+        x_embed_2 = hk.remat(clip.VisualTransformer(128, 16, 128, 2, 2, 512, "ViT01"))(x_2)
+        ##############################################################
+        y2 = hk.Linear(512)(jnp.concatenate([y_1, x_embed_2], axis=1))
+        y_2 = ResMLP(4, 512, 1024, 0.1)(y2, is_training)
+        ############################################################
+        y_3 = ResMLP(4, 1024, 512, 0.1)(jnp.concatenate([y_2, y], axis=1), is_training)
+        y_3 = hk.Linear(512)(y_3)
+        ##############################################################
+        y_4 = ResMLP(4, 512, 1024, 0.1)(y_3, is_training)
+        y_4 = hk.Linear(512)(jnp.concatenate([y_4, y_2], axis=1))
+        y = ResMLP(2, 512, 512, 0.0)(y_4, is_training)
+        return y
 
 
 def diffusion_model(x, t, y, extra_args, is_training):
-    x_pred = diffusion_image_model(x, t, y, extra_args, is_training)
-    y_pred = diffusion_latent_model(x, t, y, extra_args, is_training)
+    x_pred = DiffusionImage(256)(x, t, y, extra_args, is_training)
+    y_pred = DiffusionLatent(256)(x, t, y, extra_args, is_training)
     return x_pred, y_pred
 
 
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,7 @@
 import jax
-from jax._src.random import normal
 import jax.numpy as jnp
 import haiku as hk
+from einops import repeat
 
 import sys
 import os
@@ -9,6 +9,83 @@ import os
 sys.path.append(os.getcwd()) # cursed hack
 
 from diffusion.models.clip_latent import diffusion_model
+from diffusion.utils import t_to_alpha_sigma, log_snr_to_alpha_sigma, get_sampling_schedule
+
+
+
+def make_forward_fn(model, opt, gamma):
+
+    def compute_loss(params, key, images, embeds, extra_args, is_training):
+        key, subkey = jax.random.split(key)
+        t = jax.random.uniform(subkey, images.shape[:1])
+        alphas, sigmas = t_to_alpha_sigma(t)
+        key, subkey = jax.random.split(key)
+        image_noise = jax.random.normal(subkey, images.shape)
+        embed_noise = jax.random.normal(subkey, embeds.shape)
+        noised_images = images * alphas[:, None, None, None] + image_noise * sigmas[:, None, None, None]
+        noised_embeds = embeds * alphas[:, None] + embed_noise * sigmas[:, None]
+        image_targets = image_noise * alphas[:, None, None, None] - images * sigmas[:, None, None, None]
+        embed_targets = embed_noise * alphas[:, None] - embeds * sigmas[:, None]
+        v_im, v_emb = model.apply(params, key, noised_images, t, noised_embeds, extra_args, is_training)
+        im_loss = jnp.mean(jnp.square(v_im - image_targets))
+        emb_loss = jnp.mean(jnp.square(v_emb - embed_targets))
+        #im_loss, emb_loss = host_callback.id_print((im_loss, emb_loss), what='Image, Embed')
+        return 0.5 * (im_loss + gamma * emb_loss), (im_loss, emb_loss)
+        
+
+    def train_step(params, key, inputs, embeddings, extra_args):
+        loss_grads = jax.value_and_grad(compute_loss, has_aux=True)(params, key, inputs, embeddings, extra_args, jnp.array(1))
+        loss_aux, grads = jax.tree_util.tree_map(jnp.mean, loss_grads)
+        loss, aux_data = loss_aux
+        #updates, opt_state = opt.update(grads, opt_state, params)
+        #params = optax.apply_updates(params, updates)
+        return loss, params, aux_data
+    
+    return train_step
+
+
+def make_sample_fn(model):
+    def sample_step(params, key, x_t, y_t, log_snr, log_snr_next, eta, extra_args):
+        keys = jax.random.split(key, num=3)
+        v_im, v_lat = model.apply(
+            params,
+            keys[0],
+            x_t,
+            repeat(log_snr, '-> n', n=x_t.shape[0]),
+            y_t,
+            extra_args,
+            jnp.array(0)
+        )
+        alpha, sigma = log_snr_to_alpha_sigma(log_snr)
+        alpha_next, sigma_next = log_snr_to_alpha_sigma(log_snr_next)
+        pred_im = x_t * alpha - v_im * sigma
+        eps_im = x_t * sigma + v_im * alpha
+        pred_lat = y_t * alpha - v_lat * sigma
+        eps_lat = y_t * sigma - v_lat * alpha
+        ddim_sigma = eta * jnp.sqrt(sigma_next**2 / sigma**2) * jnp.sqrt(1 - alpha**2 / alpha_next**2)
+        adjusted_sigma = jnp.sqrt(sigma_next**2 - ddim_sigma**2)
+        x_t = pred_im * alpha_next + eps_im * adjusted_sigma + jax.random.normal(keys[1], x_t.shape) * ddim_sigma
+        y_t = pred_lat * alpha_next + eps_lat * adjusted_sigma + jax.random.normal(keys[2], y_t.shape) * ddim_sigma
+        return x_t, y_t, pred_im
+
+    def sample(params, key, x_t, y_t, steps, eta, extra_args):
+        log_snrs = get_sampling_schedule(steps)
+        sample_fn = jax.jit(sample_step)
+        for i in range(steps-1):
+            key, subkey = jax.random.split(key)
+            x_t, y_t, pred = sample_fn(
+                params,
+                subkey,
+                x_t,
+                y_t,
+                log_snrs[i],
+                log_snrs[i + 1],
+                eta,
+                extra_args
+            )
+        return pred
+
+    return sample
 
 
 def test_model():
@@ -36,12 +113,36 @@ def test_model():
     def f(rng, image, time, embed):
         v_emb = model.apply(params, rng, image, time, embed, {}, jnp.array(0))
         return v_emb
-        
+    forward = make_forward_fn(model, None, 1.9)
+
     #output = model.apply(params, keys[3], fake_image, fake_time, fake_embed, {}, jnp.array(0))
-    f(keys[3], fake_image, fake_time, fake_embed)
+    forward(params, keys[3], fake_image, fake_embed, {})
     #print(output)
     print('Model parameters:', hk.data_structures.tree_size(params))
 
 
+def test_sample():
+    shape = (3, 256, 256)
+    seed = 42
+    key = jax.random.PRNGKey(seed)
+    key, subkey = jax.random.split(key)
 
-test_model()
+    model = hk.transform(diffusion_model)
+    params = model.init(
+        subkey,
+        jnp.zeros([1, *shape]),
+        jnp.zeros([1]),
+        jnp.zeros([1, 512]),
+        {},
+        jnp.array(0)
+    )
+    keys = jax.random.split(key, num=3)
+    do_sample = make_sample_fn(model)
+    fake_image = jax.random.normal(keys[0], [3, 3, 256, 256])
+    fake_embed = jax.random.normal(keys[1], [3, 512])
+    do_sample(params, keys[2], fake_image, fake_embed, 100, 2, {})
+
+
+
+# test_model()
+test_sample()
